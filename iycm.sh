70f3fcb71a8b130575e4757f4781beaef5477e3a
gamegrd <gamegrd@gmail.com>
2016-12-24 14:31:54 +0800

    add iycm

--- iycm.sh
+++ iycm.sh
@@ -13,10 +13,19 @@ echo "If error,you need to compile it yourself"
 echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer"
 
 cd $CURRENT_DIR/bundle/
-git clone https://github.com/Valloric/YouCompleteMe.git
+ycm=$CURRENT_DIR/bundle/YouCompleteMe/
+echo "$ycm"
+
+while  [ ! -d "$ycm" ];
+do
+    git clone https://github.com/Valloric/YouCompleteMe.git
+done
 
 cd $CURRENT_DIR/bundle/YouCompleteMe/
-git submodule update --init --recursive
+for (i=1;i<1024;i++)
+do
+    git submodule update --init --recursive
+done
 if [ `which clang` ]   # check system clang
 then
     python install.py --clang-completer --system-libclang   # use system clang
